72793048c9e383e562c31c929d55da2dd5aa9abb
ahsan-storm <ahsanmuhammad1@outlook.com>
2018-11-08 13:38:02 +0500

    adds simple routing rule module for route53

---
+++ modules/route53/record/route/variables.tf
@@ -0,0 +1,16 @@
+variable "zone_id" {
+    default = ""
+}
+variable "name" {
+    default = ""
+}
+variable "type" {
+    default = "A"
+}
+variable "ttl" {
+    default = "300"
+}
+variable "records" {
+    default = []
+}
+
